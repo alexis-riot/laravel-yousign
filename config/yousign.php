bdf313b7a98b5fce71c88b51d78da605247aa3eb
Marc Bellêtre <marc@pixelparfait.fr>
2022-01-19 12:32:17 +0100

    Fix default config file

--- config/yousign.php
+++ config/yousign.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'api_key' => env('YOUSIGN_API_KEY', 'production'),
-    'api_env' => env('YOUSIGN_API_ENV'),
+    'api_env' => env('YOUSIGN_API_ENV', 'production'),
+    'api_key' => env('YOUSIGN_API_KEY'),
 ];
